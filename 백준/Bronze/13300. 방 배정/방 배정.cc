3ef426c4d14d4d641a2e1a58cc3516f76d845337
Gyeongtae Kim <127713989+kimgt0128@users.noreply.github.com>
2024-03-13 14:16:58 +0900

    [Bronze II] Title: 방 배정, Time: 0 ms, Memory: 2020 KB, Score: 2 point -BaekjoonHub

---
+++ 백준/Bronze/13300. 방 배정/방 배정.cc
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+//성별별로 학년을 저장하기 위한 배열/
+int man[7];
+int woman[7];
+int N, K, S, Y;
+int cnt;
+int main(void) {
+	cin >> N >> K;
+	//input
+	while (N--) {
+		cin >> S >> Y;
+		if (S) man[Y]++;
+		else woman[Y]++;
+	}
+	//count: 한 번의 반복문으로 해결해도 됨!
+	for (auto e : man) cnt += (e + 1) / 2;
+	for (auto e : woman)cnt += (e + 1) / 2;
+
+	cout << cnt;
+	return 0;
+}
