4933ea9aacc22c836e7eecabf18647e698aba981
Gyeongtae Kim <127713989+kimgt0128@users.noreply.github.com>
2024-04-03 20:41:42 +0900

    [Gold V] Title: 집합의 표현, Time: 32 ms, Memory: 6324 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.cc
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.cc
@@ -1,59 +1,39 @@
 #include<iostream>
 
 using namespace std;
-int N, M;
+
 int par[1000001];
-int ran[1000001];
 
 int Find(int x) {
-	return par[x] == x ? x : par[x] = Find(par[x]);
+	if (par[x] == x) return x;
+	else return par[x] = Find(par[x]);
 }
 
 void Union(int x, int y) {
-	if (Find(x) == x) par[y] = x;
-	else par[y] = Find(x);
+	x = Find(x);
+	y = Find(y);
+	if (x < y) par[y] = par[x];
+	else  par[x] = y;
 }
 
-void Rank(int x, int y) {
-	x = Find(x), y = Find(y);
-	if (ran[x] < ran[y]) {
-		Union(y, x);
-		ran[y] += ran[x];
-	}
-	else {
-		Union(x, y);
-		ran[x] = y;
-	}
-	
-}
-
-bool isUnion(int x, int y) {
-	if (Find(x) == Find(y)) return true;
-	else return false;
+void IsUnion(int x, int y) {
+	if (Find(x) == Find(y)) cout << "YES\n";
+	else cout << "NO\n";
 }
 
 int main(void) {
 	cin.tie(0);
 	ios_base::sync_with_stdio(0);
-	cin >> N >> M;
-	for (int i = 1; i <= N; i++) par[i] = i,ran[i]++;
-	while (M--) {
-		int order;
-		cin >> order;
-		int a, b;
-		cin >> a >> b;
-		if (!order) {
-			Rank(a, b);
-		}
-		else {
-			if (isUnion(a, b)) cout << "YES\n";
-			else cout << "NO\n";
-		}
+
+	int n, m;
+	cin >> n >> m;
+	for (int i = 1; i <= n; i++) par[i] = i;
+	while (m--) {
+		int order, a, b;
+		cin >> order >> a >> b;
+		if (!order) Union(a, b);
+		else IsUnion(a, b);
 	}
 
-
-
-
-	
 	return 0;
 }
