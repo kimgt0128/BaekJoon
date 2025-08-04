5978f9ef2495c803036892288322313aaae715a1
Gyeongtae Kim <127713989+kimgt0128@users.noreply.github.com>
2025-08-04 22:57:10 +0900

    [Gold II] Title: 보석 도둑, Time: 164 ms, Memory: 20580 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.cc
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.cc
@@ -1,45 +1,48 @@
 #include<iostream>
 #include<vector>
-#include<set>
+#include<queue>
 #include<algorithm>
 #define FAST_IO cin.tie(0), cout.tie(0), ios_base::sync_with_stdio(0)
 using namespace std;
 
 
 int n, k;
-vector<pair<long, int>> items;
-multiset<int> ms;
+vector<pair<int, long long>> items;
+vector<int> bags;
+priority_queue<long long> pq;
 long long ans;
 
-
 int main(void) {
-
 	FAST_IO;
 
 	cin >> n >> k;
 
 	for (int i = 0; i < n; i++) {
-		long m, v;
+		long long m, v;
 		cin >> m >> v;
-		items.push_back({ v,m });
+		items.push_back({ m, v });
 	}
-
 	for (int i = 0; i < k; i++) {
 		int w;
 		cin >> w;
-		ms.insert(w);
+		bags.push_back(w);
 	}
 
-	sort(items.begin(), items.end(), greater<pair<int, int>>());
+	sort(items.begin(), items.end());
+	sort(bags.begin(), bags.end());
 
-	for (int i = 0; i < n; i++) {
-		auto bag_idx = ms.lower_bound(items[i].second);
-		if (bag_idx == ms.end()) continue;
-		ans += items[i].first;
-		ms.erase(bag_idx);
+	int tmp = 0;
+	for (int i = 0; i < k; i++) {
+		int cur_weight = bags[i];
+		
+		while (tmp < n && items[tmp].first <= cur_weight) {
+			pq.push(items[tmp].second);
+			tmp++;
+		}
+		if (!pq.empty()) ans += pq.top(), pq.pop();
 
 	}
+
 	cout << ans;
-
 	return 0;
 }
